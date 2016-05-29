a712e2e8f3ba487a74eee37d76f468e52ffa35ab
Douglas Morrow <douglas.p.morrow@gmail.com>
2016-05-28 18:53:53 -0700

    Add typings from typed-ndarray

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,4 @@
-declare function zeros(shape: number[], dtype?: string) : any;
+import {NDArray} from 'ndarray';
+declare function zeros(shape: number[], dtype?: string) : NDArray;
 export = zeros;
+
--- test/test.ts
+++ test/test.ts
@@ -1,3 +1,20 @@
 import test = require('blue-tape');
-
 import zeros = require('zeros');
+
+test('module can load properly', null, function(t) {
+  var test = zeros([1,1]);
+
+  t.notEqual(null, test, 'created instance is not null');
+  t.notEqual(undefined, test.set, 'has expected method: set ');
+  t.end();
+});
+
+test('can get a value that was set', null, function(t) {
+  var test = zeros([1,1]);
+  var value = 34;
+
+  test.set(0,0, value);
+
+  t.equal(test.get(0,0), value);
+  t.end();
+});
